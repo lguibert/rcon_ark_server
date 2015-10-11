5a40fd7b0253f73563632bb2989a186d0e6c3625
Lucas <lucas.guibert@viacesi.fr>
2015-10-11 15:38:09 +0200

    user management

--- server/views/users_views.py
+++ server/views/users_views.py
@@ -1,13 +1,21 @@
 from general_views import send_response
 from django.views.decorators.csrf import csrf_exempt
 import json
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
 
 
 @csrf_exempt
 def login(request):
     if request.method == "POST":
-        id = request.POST.get('login', False)
-        password = request.POST.get('password', False)
-        return send_response([1, 1, "admin"])
+        data = json.loads(request.body)
+        user = authenticate(username=data['username'], password=data['password'])
+        if user is not None:
+            if user.is_superuser:
+                return send_response([user.id, user.id, "admin"])
+            else:
+                return send_response("Pas d'utilisateur 1.", 500)
+        else:
+            return send_response("Pas d'utilisateur 2.", 500)
     else:
-        return send_response([1, 1, "admin"])
+        return send_response("Pas d'utilisateur 3.", 500)
