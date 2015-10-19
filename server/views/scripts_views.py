01569ec462f40698b574964f2315851e2d44c4ad
Lucas <lucas.guibert@viacesi.fr>
2015-10-19 23:29:04 +0200

    correction

--- server/views/scripts_views.py
+++ server/views/scripts_views.py
@@ -9,7 +9,7 @@ import requests
 def execute_script(request):
     #if request.method == "POST":
     data = json.loads(request.body)
-    r = requests.get("http://" + settings.SERVER + ":" + str(settings.PORT_SCRIPT) + "/" + data[0])
-    return send_response(r.text)
+    #r = requests.get("http://" + settings.SERVER + ":" + str(settings.PORT_SCRIPT) + "/" + data[0])
+    return send_response(data) #r.text
     #else:
     #    return send_response("")
