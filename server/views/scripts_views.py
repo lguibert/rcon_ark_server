a7d6d1c9821afa2d8a38bda26de4167a56eec54c
lguibert <apqmwn33>
2015-10-20 12:54:50 +0200

    timeout

--- server/views/scripts_views.py
+++ server/views/scripts_views.py
@@ -9,7 +9,7 @@ import requests
 def execute_script(request):
     #if request.method == "POST":
     data = json.loads(request.body)
-    #r = requests.get("http://" + settings.SERVER + ":" + str(settings.PORT_SCRIPT) + "/" + data[0])
-    return send_response("http://" + settings.SERVER + ":" + str(settings.PORT_SCRIPT) + "/" + data[0]) #r.text
+    r = requests.get("http://" + settings.SERVER + ":" + str(settings.PORT_SCRIPT) + "/" + data[0])
+    return send_response(r.text) #r.text
     #else:
     #    return send_response("")
